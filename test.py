4f8ca26b8cd2db8cfc7f5c1eb52b4c9e3585cddc
ZerQAQ <1075843852@qq.com>
2020-05-11 21:01:56 +0800

    delete test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-import config
-
-db = pymysql.connect(**config.sql)
-c = db.cursor()
-c._execute("select nick from user where id = 1 and password = 1")
-c.close()
-db.commit()
-print(tuple([1]))
